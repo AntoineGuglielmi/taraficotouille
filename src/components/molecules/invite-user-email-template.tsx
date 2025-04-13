b205d49a244eaf83dcf1546a526fe47d81e3b1d8
Antoine Guglielmi <contact.antoine.guglielmi@gmail.com>
2025-04-14 00:26:55 +0200

    [TARA-NI] Use env variable in mail template

--- src/components/molecules/invite-user-email-template.tsx
+++ src/components/molecules/invite-user-email-template.tsx
@@ -7,10 +7,11 @@ export default function InviteUserEmailTemplate({
   className,
   id,
 }: InviteUserEmailTemplateProps) {
+  const { HOST } = process.env
   return (
     <div className={` ${className}`}>
       <p>
-        Clique sur <a href={`http://localhost:3000/adminify/${id}`}>ce lien</a>{' '}
+        Clique sur <a href={`http://${HOST}:3000/adminify/${id}`}>ce lien</a>{' '}
         pour pouvoir inventer des mots toi aussi :)
       </p>
     </div>
